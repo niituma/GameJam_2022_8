81fc41ac0c8e56fa3acb579a3c571223be71f600
Ray <rei.piyo.55687@gmail.com>
2022-08-18 18:50:02 +0900

    通行人続き

--- Assets/Yuki/WalkerMove.cs
+++ Assets/Yuki/WalkerMove.cs
@@ -9,37 +9,35 @@ using UnityEngine.UI;
 /// 
 public class WalkerMove : MonoBehaviour
 {
-    [Tooltip("左方向制限範囲")] float _leftMove;
-    [Tooltip("右方向制限範囲")] float _rightMove;
-    [Tooltip("上方向制限範囲")] float _topMove;
-    [Tooltip("下方向制限範囲")] float _bottomMove;
+    [SerializeField, Tooltip("左方向制限範囲")] float _leftMove;
+    [SerializeField, Tooltip("右方向制限範囲")] float _rightMove;
+    [SerializeField, Tooltip("上方向制限範囲")] float _topMove;
+    [SerializeField, Tooltip("下方向制限範囲")] float _bottomMove;
     [Tooltip("通行人ポジション")] Transform _walkerPositon;
     [Tooltip("前回移動時のX座標")] float _oldX;
-    Vector3 _walkerScale;
+    Vector2 _walkerScale;
+    [SerializeField, Header("歩行スピード")] float _walkerSpeed;
+    Vector2 _moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         _walkerPositon = GetComponent<Transform>();
-        _oldX = _walkerScale.x;
+        _oldX = _walkerPositon.position.x;
+        _moveDirection = MoveRandomPosition();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        //通行人の移動＆跳ね返り処理
-        
-
-
-
         //前回のX座標からどちらに動いたかで向きを決定
-        _walkerScale = transform.localScale;
-        if (_walkerPositon.position.x < _oldX && 0 <= _walkerScale.x)   //左に移動 かつ 現在スプライトが右向きだったら、左向きに変更
+        _walkerScale = this.transform.localScale;
+        if (_walkerPositon.position.x <= _oldX && 1 == _walkerScale.x)   //左に移動 かつ 現在スプライトが右向きだったら、左向きに変更
         {
             _walkerScale.x = -1;
         }
-        else if (_walkerPositon.position.x > _oldX && _walkerScale.x < 0)   //右に移動 かつ 現在スプライトが左向きだったら、右向きに変更
+        else if (_walkerPositon.position.x > _oldX && _walkerScale.x == -1)   //右に移動 かつ 現在スプライトが左向きだったら、右向きに変更
         {
             {
                 _walkerScale.x = 1;
@@ -49,5 +47,23 @@ public class WalkerMove : MonoBehaviour
         {
             return;
         }
+
+        //通行人の移動＆跳ね返り処理
+        if (_moveDirection.x == _walkerPositon.position.x && _moveDirection.y == _walkerPositon.position.y)  //playerオブジェクトが目的地に到達すると、
+        {
+            _moveDirection = MoveRandomPosition();  //目的地を再設定
+        }
+        this.transform.position = Vector2.MoveTowards(this.transform.position, _moveDirection, _walkerSpeed * Time.deltaTime);
+
+
+
+        _oldX = _walkerPositon.position.x;
+    }
+
+
+    private Vector2 MoveRandomPosition()  // 目的地を生成、xとyのポジションをランダムに値を取得 
+    {
+        Vector2 _randomPosi = new Vector2(Random.Range((int)_leftMove, (int)_rightMove), Random.Range((int)_bottomMove, (int)_topMove));
+        return _randomPosi;
     }
 }
