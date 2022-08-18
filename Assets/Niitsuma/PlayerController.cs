8f45aa979f94b82991283bc33becef5bf66909ca
sakezuma <sakezuma@gmail.com>
2022-08-18 17:05:10 +0900

    近くのアクションできるもの判別できるようにした

--- Assets/Niitsuma/PlayerController.cs
+++ Assets/Niitsuma/PlayerController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float _speed = 3f;
+    [SerializeField]
+    float _radius = 3;
 
     float _h, _v;
     Vector2 _dir;
@@ -24,7 +27,7 @@ public class PlayerController : MonoBehaviour
         _v = Input.GetAxisRaw("Vertical");
         if (Input.GetButtonDown("Fire1"))
         {
-            Action();
+            if (SearchAreaInEnemies()) { Action(); }
         }
         if (Input.GetButtonDown("Fire2"))
         {
@@ -32,7 +35,26 @@ public class PlayerController : MonoBehaviour
         }
         _dir = new Vector2(_h, _v);
     }
+    void FixedUpdate()
+    {
+        float speed = _dir == Vector2.zero ? 0 : _speed;
+        _rb.velocity = _dir.normalized * speed;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
 
+    GameObject SearchAreaInEnemies()
+    {
+        var Obj 
+            = Physics2D.OverlapCircleAll(transform.position, _radius).Where(e => e.tag == "Finish").OrderBy(e => Vector2.Distance(e.transform.position,this.transform.position)).FirstOrDefault();
+
+        if(Obj == null) { return null; }
+
+        return Obj.gameObject;
+    }
     void Action()
     {
         Debug.Log("Action");
@@ -43,9 +65,4 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Panch");
     }
 
-    private void FixedUpdate()
-    {
-        float speed = _dir == Vector2.zero ? 0 : _speed;
-        _rb.velocity = _dir.normalized * speed;
-    }
 }
