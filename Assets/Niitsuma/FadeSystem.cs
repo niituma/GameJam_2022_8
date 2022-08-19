1869706f0c6996f25b85bfac09f584bc26c5df10
sakezuma <sakezuma@gmail.com>
2022-08-19 10:04:23 +0900

    prefab化

---
+++ Assets/Niitsuma/FadeSystem.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeSystem : MonoBehaviour
+{
+    [SerializeField] float _fadeInSpeed = 0.8f;
+    [SerializeField] float _fadeOutSpeed = 0.01f;
+    bool _isFadeOut = default;
+    bool _isFadeIn = default;
+
+    [SerializeField, Tooltip("ゲーム開始時にFadeInする")] bool _isStartFadeIn = true;
+    [SerializeField] Image fadeImage;
+    [SerializeField] string _sceneName;
+    float red, green, blue;
+    float alfa;
+    SceneManagerScript _sceneChanger;
+
+    private void Start()
+    {
+        _sceneChanger = GetComponent<SceneManagerScript>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alfa = fadeImage.color.a;
+
+        if (_isStartFadeIn) { OnFadeIn(); }
+    }
+    private void Update()
+    {
+        if (_isFadeOut)
+        {
+            StartFadeOut();
+        }
+        else if (_isFadeIn)
+        {
+            StartFadeIn();
+        }
+    }
+    public void OnFadeIn()
+    {
+        fadeImage.gameObject.SetActive(true);
+        alfa = 1f;
+        _isFadeIn = true;
+    }
+    /// <summary>
+    /// FadeOutを開始する関数
+    /// </summary>
+    /// <param name="name"> Scene遷移するときのScene先の名前</param>
+    public void OnFadeOut(string name)
+    {
+        fadeImage.gameObject.SetActive(true);  // a)パネルの表示をオンにする
+        _sceneName = name;
+        alfa = 0f;
+        _isFadeOut = true;
+    }
+    void StartFadeIn()
+    {
+        alfa -= _fadeInSpeed * Time.deltaTime;                //a)不透明度を徐々に下げる
+        fadeImage.color = new Color(red, green, blue, alfa);    //b)変更した不透明度パネルに反映する
+        if (alfa <= 0)
+        {
+            _isFadeIn = false;
+            fadeImage.gameObject.SetActive(false);
+        }
+    }
+    void StartFadeOut()
+    {
+        alfa += _fadeOutSpeed * Time.deltaTime;         // b)不透明度を徐々にあげる
+        fadeImage.color = new Color(red, green, blue, alfa);    // c)変更した透明度をパネルに反映する
+        if (alfa >= 1)
+        {
+            _isFadeOut = false;  //d)パネルの表示をオフにする
+            SceneManager.LoadSceneAsync(_sceneName);
+        }
+    }
+}
