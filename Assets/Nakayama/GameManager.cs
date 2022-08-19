e2198621f0958ea04ccd7c710baa84336294c135
BlackSnow(PC) <yumi06050425@gmail.com>
2022-08-19 12:01:48 +0900

    諸々追加

--- Assets/Nakayama/GameManager.cs
+++ Assets/Nakayama/GameManager.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] static public int _ngPoint;
+    [SerializeField] Image img;
+    float _imgalpha;
+    float angle = 0;
 
     void Start()
     {
+        img.color = Color.clear;
+        _ngPoint -= 5;
+
     }
     void Update()
     {
+        if (_ngPoint < 0)
+        {
+            this.img.color = new Color(0.5f, 0f, 0f, _imgalpha);
+            _imgalpha = Mathf.Sin(angle * (Mathf.PI / 180)) / 3;
+            angle += 0.5f;
+            if (angle > 360) angle -= 360;
+        }
+        else
+        {
+            this.img.color = Color.Lerp(this.img.color, Color.clear, Time.deltaTime);
+        }
+
 
     }
     public void Good()
