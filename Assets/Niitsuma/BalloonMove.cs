3ff5d668ba542ad602e4c277369636ee4f35e8e7
sakezuma <sakezuma@gmail.com>
2022-08-19 13:54:34 +0900

    絵追加

--- Assets/Niitsuma/BalloonMove.cs
+++ Assets/Niitsuma/BalloonMove.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BalloonMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>íeÇ™îÚÇ‘ë¨Ç≥</summary>
+    [SerializeField] float _speed = 3f;
+    /// <summary>íeÇÃê∂ë∂ä˙ä‘ÅiïbÅj</summary>
+    [SerializeField] float _lifeTime = 5f;
+    [SerializeField] float _amplitube = 1.5f;
+    [SerializeField] float _SpeedY = 3f;
+    float _timer;
 
+    Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, _lifeTime);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        _timer += Time.deltaTime;
+        _rb.velocity = new Vector2(_speed, Mathf.Sin(_timer * _SpeedY) * _amplitube);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FindObjectOfType<PlayerController>().CatchBalloon();
+            Destroy(this.gameObject);
+        }
+    }
+
 }
