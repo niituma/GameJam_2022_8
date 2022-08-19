52bf4d5cd2b6b09524c589efa0c7852aeb830b52
CHARA\meich <mei.charaa@gmail.com>
2022-08-19 15:35:13 +0900

    タスク成功時の効果音、エンディングのジングルを追加しました。ふうせんとねこは音が鳴るスクリプトを書きました。

--- Assets/TamuraMei/Scripts/CatchBalloon.cs
+++ Assets/TamuraMei/Scripts/CatchBalloon.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>風船をキャッチ</summary>
+[RequireComponent(typeof(AudioSource))]
+/// <summary>風船を子供に渡す</summary>
 public class CatchBalloon : TaskBase
 {
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     public override void Action()
     {
 
         if(_player.Haveballoon)
         {
+            _audioSource.PlayOneShot(_audioSource.clip); //Balloon
             _player.PassBalloon();
             Clear();
         }
--- Assets/TamuraMei/Scripts/RescueCat.cs
+++ Assets/TamuraMei/Scripts/RescueCat.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 /// <summary>”L‚¿‚á‚ñ‚ðƒLƒƒƒbƒ`</summary>
 public class RescueCat : TaskBase
 {
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     public override void Action()
     {
 
         if(_player.HaveLadder)
         {
+            _audioSource.PlayOneShot(_audioSource.clip); //Cat
             _player.UsedLadder();
             Clear();
         }
