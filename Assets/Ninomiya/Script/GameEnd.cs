f53afc7eacab650de1110ea9165673ec85154216
ninomiyahayato <a7pseej9@icloud.com>
2022-08-19 12:20:37 +0900

    GoodEnding 絵　script追加

---
+++ Assets/Ninomiya/Script/GameEnd.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        EndGame();
+    }
+    private void EndGame() //ゲームを強制終了
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnityEditor.EditorApplication.isPlaying = false; //UnityEditorの実行を停止する
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();// ゲームを終了する
+        }
+    }
+}
